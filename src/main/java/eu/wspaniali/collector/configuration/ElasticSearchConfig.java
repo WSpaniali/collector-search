6f44d8d3f217d266ac141a7a1703ca5a45c15c71
Wojciech Odrobina <wowikro@gmail.com>
2023-12-24 15:56:24 +0100

    Initial

---
+++ src/main/java/eu/wspaniali/collector/configuration/ElasticSearchConfig.java
@@ -0,0 +1,36 @@
+package eu.wspaniali.collector.configuration;
+
+import co.elastic.clients.elasticsearch.ElasticsearchClient;
+import co.elastic.clients.json.jackson.JacksonJsonpMapper;
+import co.elastic.clients.transport.rest_client.RestClientTransport;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.apache.http.HttpHost;
+import org.elasticsearch.client.RestClient;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ElasticSearchConfig {
+
+    private final String elasticsearchAddress;
+
+    public ElasticSearchConfig(@Value("${elasticsearch.connection.address}") String address) {
+        this.elasticsearchAddress = address;
+    }
+
+    @Bean
+    ObjectMapper objectMapper() {
+        return new ObjectMapper();
+    }
+
+    @Bean
+    ElasticsearchClient elasticSearchClient() {
+        RestClient restClient = RestClient.builder(
+                        new HttpHost(elasticsearchAddress, 9200))
+                .build();
+
+        RestClientTransport transport = new RestClientTransport(restClient, new JacksonJsonpMapper());
+        return new ElasticsearchClient(transport);
+    }
+}
